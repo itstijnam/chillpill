e8e43557603912f5c3ef08b1526b38b501b981d1
itstijnam <manjitsyngh@gmail.com>
2025-07-30 19:51:31 +0530

    home

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,4 +1,4 @@
 
 
-export const baseUrl = "http://localhost:3000"
-// export const baseUrl = "https://chillpillbackend.vercel.app"
+// export const baseUrl = "http://localhost:3000"
+export const baseUrl = "https://chillpillbackend.vercel.app"
